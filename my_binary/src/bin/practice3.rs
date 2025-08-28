e31d11a82fb75466183d09bd2e1065efa98ee103
jtdocter27 <john.docter@colorado.edu>
2025-08-28 15:03:10 -0600

    Basic Syntax completed

--- my_binary/src/bin/practice3.rs
+++ my_binary/src/bin/practice3.rs
@@ -1,3 +1,9 @@
+#![allow(warnings)] //macro that gets rid of all warnings in the script.
+
+
+pub const FLOUNDER:& 'static str = "Flounder"; 
+
+
 fn main() {
     fn numbersprac() {
         let three: i8 = 3; 
@@ -22,14 +28,35 @@ interior();
     fn constantinople() { 
         const POKEMON: &str = "Bulbasaur"; //must be screaming snake case and requires a type declaration, 
         const SECONDS_IN_A_DAY: usize = 60*60*25; //constants can be assigned to any expression. usize is an unsigned integer that autmatically macthes memory requirements.  
-        println!("{} {}", POKEMON, SECONDS_IN_A_DAY); 
-
-
-
-
-
+        // println!("{} {}", POKEMON, SECONDS_IN_A_DAY); 
     }
 constantinople();
 
+pub mod cake {
+    pub fn favorite(name: &str) -> bool {
+        name == "Coconut"
+    } // public function for comparing a literal string Coconut to whatever is inputted. Will return true or false, hence the "bool" portion of this
+    
+}
+let guess = cake::favorite("Marble");  //this is path syntax - essentially a way to access a function in a module
+println!("{}", guess);
 
+mod ocean {
+    pub const ATLANTIC: &'static str = "Atlantic"; //creates a public constant called ATLANTIC that is a global variable
+
+    pub mod fish {
+        pub fn print_flounder() {
+            use crate::FLOUNDER;
+            use super::ATLANTIC; 
+
+            println!("A {FLOUNDER} in the {ATLANTIC}");
+
+
+            }
+            
+        }
+    } 
+ocean::fish::print_flounder(); 
+let x = 1; 
+assert!(x>0, "Failed"); 
 }
