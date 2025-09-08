f7cf122fcba07329787f7cbe50189113aefbd45c
jtdocter27 <john.docter@colorado.edu>
2025-09-08 17:10:03 -0600

    Through traits and generics

--- my_binary/src/bin/traits_and_generics.rs
+++ my_binary/src/bin/traits_and_generics.rs
@@ -1,5 +1,7 @@
 fn main() {
    use std::fmt::Display; // brings the Display trait into scope from the standard library so it can be used. 
+   use std::fmt::Debug; 
+
    const TEAMMATE: &'static str = "Cooperberg"; // declares a constant as a static string. This means TEAMMATE is immutable and will live for the lifetime of the program 
 
    pub fn pass_to<'a, T: Display>(name: &'a T) { //this declares a function called pass to that has a lifetime annotation and takes in any data type. 
@@ -30,23 +32,79 @@ fn main() {
     println!("{:?}", pairs); 
     println!("{:?}", tree);
 
-    // Traitss and Generics -----------------------------------------------------------------
+    // Traits -----------------------------------------------------------------
 
     trait Describable {
         fn describe(&self) -> String; //This is a method. &self is just a generic way of saying "input some data (instance) here" and then the function returns a string. 
         fn summary(&self) -> String {
             format!("Summary: {}", self.describe()) //macro that creates formatted strings and returns it. 
+        }}
+
+    struct Organism {
+        name: String, 
+        species: String, 
+    }
+    impl Describable for Organism {  
+        fn describe(&self) -> String {
+            format!("{}, {}", self.name, self.species)
         }
-        struct Organism {
-            name: String, 
-            species: String, 
-        }
+    }
+    let ecoli = Organism {
+        name: "E. coli".to_string(), 
+        species: "coli".to_string(), 
+    }; 
+    println!("{}", ecoli.describe()); 
+    println!("{}", ecoli.summary()); 
 
 
+// Generics ------------------------------------------------------------------------------------------------
+
+ 
+#[derive(Debug)]
+struct Velocity<T>(T); 
+
+let v_int = Velocity(5); 
+let v_float = Velocity(2.8); 
+
+
+println!("{:?} {:?}", v_int, v_float); 
+
+
+fn debug_sound<T: Display + Debug>(noise: T) {
+    println!("normal: {noise}"); 
+    println!("debug: {noise:?}"); 
+}
+
+debug_sound("Slam"); 
+
+//or, in an easier fashion...
+    fn sound(noise: impl Display) {
+        println!("{noise}"); 
+    }
+sound("Blam"); 
+
+
+fn example_of_where<T, U>(noise: T, room: U) -> T 
+where
+    T: Display + Clone, 
+    U: Debug + Copy, 
+    { 
+        println!("{noise} in the {room:?}"); 
+        noise.clone() 
+    }
+
+//turbofish -------------------------------------------------
+//function_name::<Type>(arguments)
+let x = "42".parse::<i32>().unwrap(); 
+let x: i32 = "42".prase().unwrap(); 
+
+//turbofish is a specific kind of type inference. It can be used in place of the normal type declaration (as seen above) 
+// or it can be used in specific situations. 
+"1,2,3".split(',').map(|s| s.parse::<i32>().unwrap()).collect::<Vec<_<<()
+//take the string 1,2,3, split it at the comma to create each integer, apply (map) the .parse, which converts each chunk into an i32, unwrap extracts the integers, collect consumes the iterator and builds the collect
     }
 
 
 
 
 
-}
